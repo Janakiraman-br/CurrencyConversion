d778ab364a8be23eecf26027e074c7d2c171090e
Janaki Raman <janaki.raman@maersk.com>
2021-08-08 15:11:14 +0530

    Updated GetCodeAndName

--- CurrencyConversion/src/main/java/net/apmoller/athena/microservices/CurrencyProject/controller/ExceptionAdvice.java
+++ CurrencyConversion/src/main/java/net/apmoller/athena/microservices/CurrencyProject/controller/ExceptionAdvice.java
@@ -10,19 +10,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionAdvice
 {
-//    @ExceptionHandler(CurrencyConversionException.class)
-//    public ResponseEntity<CurrencyConversionError> mapException(CurrencyConversionException ex)
-//    {
-//        CurrencyConversionError details = new CurrencyConversionError("404 NOT FOUND", ex.getMessage());
-//        return new ResponseEntity(details, HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<CurrencyConversionError> mapExceptions(Exception e)
-//    {
-//        CurrencyConversionError error = new CurrencyConversionError("Something Happened", "Please check and correct it");
-//        return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
+    @ExceptionHandler(CurrencyConversionException.class)
+    public ResponseEntity<CurrencyConversionError> mapException(CurrencyConversionException ex)
+    {
+        CurrencyConversionError details = new CurrencyConversionError("404 NOT FOUND", ex.getMessage());
+        return new ResponseEntity(details, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CurrencyConversionError> mapExceptions(Exception e)
+    {
+        CurrencyConversionError error = new CurrencyConversionError("Something Happened", "Please check and correct it");
+        return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 
 }
